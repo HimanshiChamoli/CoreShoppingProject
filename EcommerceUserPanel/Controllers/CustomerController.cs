a5b1c229aaa496e86ff12b0e438aab745b6646a7
Himanshi <Himanshi.3@NIIT-Tech.com>
2019-03-20 11:28:58 +0530

    first commit

---
+++ EcommerceUserPanel/Controllers/CustomerController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceUserPanel.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceUserPanel.Controllers
+{
+
+    public class CustomerController : Controller
+    {
+        ShoppingDemoooo2Context context = new ShoppingDemoooo2Context();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Customers customer)
+        {
+            context.Customers.Add(customer);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        //[Route("Login")]
+        [HttpPost]
+
+        public ActionResult Login(string Password, string Username)
+        {
+            var user = context.Customers.Where(x => x.Username == Username).SingleOrDefault();
+            ViewBag.cust = user;
+            if (user == null)
+            {
+                ViewBag.Error = "Invalid Credentials";
+                return View("Login");
+            }
+            else
+            {
+                var userName = user.Username;
+                int custId = ViewBag.cust.CustomerId;
+                if (userName != null && Password != null && userName.Equals(userName)
+                    && Password.Equals("12345"))
+                {
+                    HttpContext.Session.SetString("uname", userName);
+                    return RedirectToAction("Checkout", "Cart", new { @id = custId });
+                }
+                else
+                {
+                    ViewBag.Error = "Invalid Credentials";
+                    return View("Login");
+                }
+            }
+        }
+    }
+}
---
+++ EcommerceUserPanel/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EcommerceUserPanel.Models;
+using EcommerceUserPanel.Helpers;
+using Microsoft.AspNetCore.Http;
+
+namespace EcommerceUserPanel.Controllers
+{
+    public class HomeController : Controller
+    {
+        ShoppingDemoooo2Context context = new ShoppingDemoooo2Context();
+        public IActionResult Index()
+        {
+            var product = context.Products.ToList();
+            int j = 0;
+            var cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+            int i = 0;
+            if (cart != null)
+            {
+                foreach (var item in cart)
+                {
+                    i++;
+                }
+                if (i != 0)
+                {
+                    foreach (var i1 in cart)
+                    {
+                        j++;
+                    }
+                    HttpContext.Session.SetString("cartitem", j.ToString());
+                }
+            }
+            return View(product);
+        }
+        public IActionResult AboutUs()
+        {
+            
+            return View();
+        }
+
+
+    }
+}
